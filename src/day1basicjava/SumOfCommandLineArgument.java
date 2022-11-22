c20b0202db0a9d5672f1d9984e9a22f028bfaaeb
hiteshptl97 <hiteshpatil897@gmail.com>
2022-11-22 20:47:02 +0530

    sum command line argument

---
+++ src/day1basicjava/SumOfCommandLineArgument.java
@@ -0,0 +1,28 @@
+package day1basicjava;
+
+public class SumOfCommandLineArgument {
+    public static void main(String[] args) {
+
+        int n, cnt=0, sum=0;
+        for(int i=0;i<args.length;i++)
+        {
+            try
+            {
+                n=Integer.parseInt(args[i]);
+                sum= sum+n;
+            }
+            catch (Exception e)
+            {
+
+                cnt++;
+            }
+        }
+        System.out.println("Sum of only valid integers is= "+sum) ;
+
+        System.out.println("Invalid integers are: "+cnt) ;
+    }
+
+
+}
+
+
