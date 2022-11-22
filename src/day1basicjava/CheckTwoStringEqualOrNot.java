b69a6a1d7c3320a6bf266f898b7836cf498ea606
hiteshptl97 <hiteshpatil897@gmail.com>
2022-11-22 20:44:12 +0530

    check the two string equal or not

---
+++ src/day1basicjava/CheckTwoStringEqualOrNot.java
@@ -0,0 +1,19 @@
+package day1basicjava;
+
+public class CheckTwoStringEqualOrNot {
+
+    public static void main(String[] args) {
+
+        String s1="HITESH";
+        String s2="NILESH";
+
+
+        if (s1.equals(s2))
+            System.out.print("Equal Strings");
+        else
+            System.out.print("UnEqual Strings");
+    }
+
+}
+
+
