c3fa7281a97ed3ba6f8fca98b42b736ec8090561
hiteshptl97 <hiteshpatil897@gmail.com>
2022-11-22 20:53:35 +0530

    demonstrete static variable method

---
+++ src/day1basicjava/DemonStrateStaticVariable.java
@@ -0,0 +1,19 @@
+package day1basicjava;
+
+public class DemonStrateStaticVariable {
+    static int x = 20;
+    static int y;
+    static void func(int z) {
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("z = " + z);
+    }
+    static {
+        System.out.println("Running static initialization block.");
+        y = x + 7;
+    }
+    public static void main(String[] args) {
+        func(9);
+
+    }
+}
