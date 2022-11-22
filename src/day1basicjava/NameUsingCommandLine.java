6286d68b26ab9cc30000f8dfc596976734e30956
hiteshptl97 <hiteshpatil897@gmail.com>
2022-11-22 20:49:47 +0530

    name using command line

---
+++ src/day1basicjava/NameUsingCommandLine.java
@@ -0,0 +1,9 @@
+package day1basicjava;
+
+public class NameUsingCommandLine {
+
+    public static void main(String[] args) {
+        System.out.println("Your first argument is: "+args[0]);
+
+    }
+}
